4fc0b0d1c397132bdf37a22ebf5f85ccd3e72493
Elisha Bere <elishabere4@gmail.com>
2022-07-22 19:11:48 +0200

    add frontend and update contract deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,24 +1,57 @@
 const { ethers } = require("hardhat");
 
+const path = require("path");
+
 async function main() {
-  /*
-  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-  so mainBankContract here is a factory for instances of our Defi Bank contract.
-  */
-  const mainBankContract = await ethers.getContractFactory("Main");
+  // This is just a convenience check
+  if (network.name === "hardhat") {
+    console.warn(
+      "You are trying to deploy a contract to the Hardhat Network, which" +
+        "gets automatically created and destroyed every time. Use the Hardhat" +
+        " option '--network localhost'"
+    );
+  }
 
-  const deployedMainBankContract = await mainBankContract.deploy();
-
-  await deployedMainBankContract.deployed();
-
-  // print the address of the deployed contract
+  // ethers is available in the global scope
+  const [deployer] = await ethers.getSigners();
   console.log(
-    "Main Bank Contract Address:",
-    deployedMainBankContract.address
+    "Deploying the contracts with the account:",
+    await deployer.getAddress()
+  );
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const BankToken = await ethers.getContractFactory("Token");
+  const bankToken = await BankToken.deploy();
+  await bankToken.deployed();
+
+  console.log("Token Bank address:", bankToken.address);
+
+  // We also save the contract's artifacts and address in the frontend directory
+  saveFrontendFiles(bankToken);
+}
+
+function saveFrontendFiles(token) {
+  const fs = require("fs");
+  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    path.join(contractsDir, "contract-address.json"),
+    JSON.stringify({ Token: token.address }, undefined, 2)
+  );
+
+  const TokenArtifact = artifacts.readArtifactSync("Token");
+
+  fs.writeFileSync(
+    path.join(contractsDir, "BankToken.json"),
+    JSON.stringify(TokenArtifact, null, 2)
   );
 }
 
-// Call the main function and catch if there is any error
 main()
   .then(() => process.exit(0))
   .catch((error) => {
